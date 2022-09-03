512fea27af4a67b6f32af5e558da7477b6168108
netbek932 <rjeren22@gmail.com>
2022-09-03 15:28:44 -0400

    Complete Bare Minimum Requirements

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -5,7 +5,7 @@ const RepoList = (props) => (
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos:
     <ul>
-      {props.repos.map((repo) => <li key={repo.repo_id}><a href={repo.repo_url} target="_blank">{repo.repo_name}</a> </li>)}
+      {props.repos.map((repo) => <li key={repo.repo_id}><a href={repo.repo_url} target="_blank">{repo.repo_name}</a> {repo.stargazers_count}</li>)}
     </ul>
   </div>
 )
